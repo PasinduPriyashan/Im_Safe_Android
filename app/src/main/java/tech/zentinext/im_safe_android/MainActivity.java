9aabe430385ba627909cb920380a4266a49e781b
PasinduPriyashan <pasindu95.m@gmail.com>
2017-09-21 17:19:36 +0530

    Added Ignore File

--- app/src/main/java/tech/zentinext/im_safe_android/MainActivity.java
+++ app/src/main/java/tech/zentinext/im_safe_android/MainActivity.java
@@ -14,6 +14,17 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -90,7 +101,51 @@ class LocationTracker extends AsyncTask{
                 while((boolean)params[1]){
                 MainActivity mainActivity = (MainActivity) params[0];
                 mainActivity.getLoco();
-                    System.out.println("New Location");
+                    if (mainActivity.mylastLocation!=null) {
+                        String link = "https://imsafe.cf/location";
+
+                        JSONArray jsonArray = new JSONArray();
+                        JSONObject jsonObject = new JSONObject();
+                        jsonObject.put("name", "id");
+                        jsonObject.put("value", 1296);
+                        jsonArray.put(jsonObject);
+                        JSONObject jsonObject1 = new JSONObject();
+                        jsonObject1.put("name", "lat");
+                        jsonObject1.put("value", mainActivity.mylastLocation.getLatitude());
+                        jsonArray.put(jsonObject1);
+                        JSONObject jsonObject2 = new JSONObject();
+                        jsonObject2.put("name", "lon");
+                        jsonObject2.put("value", mainActivity.mylastLocation.getLongitude());
+                        jsonArray.put(jsonObject2);
+
+
+                        String data = URLEncoder.encode("attr", "UTF-8")+"="+URLEncoder.encode(jsonArray.toString(), "UTF-8");
+                        URL url = new URL(link);
+                        URLConnection conn = url.openConnection();
+                        //System.out.println(data);
+                        conn.setDoOutput(true);
+                        OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+
+                        wr.write(data);
+                        wr.flush();
+
+                        BufferedReader reader = new BufferedReader(new
+                                InputStreamReader(conn.getInputStream()));
+
+                        StringBuilder sb = new StringBuilder();
+                        String line = null;
+
+                        // Read Server Response
+                        while ((line = reader.readLine()) != null) {
+                            sb.append(line);
+                            break;
+                        }
+                        String results = sb.toString();
+                        if (results.length() == 0) {
+
+                        }
+                        System.out.println(results);
+                    }
                 Thread.sleep(4000);}
                 return null;
             }catch (Exception e){
